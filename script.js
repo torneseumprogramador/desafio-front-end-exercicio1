e2306610d583d518a19ca434d65c0842be924abd
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-06-14 06:58:03 -0300

    aula 12 menu flutuante

--- script.js
+++ script.js
@@ -21,6 +21,18 @@ function proximo(){
     document.querySelector('#carrossel div').scrollLeft += window.innerWidth
 }
 
+function abrirMenu(button){
+    let menu = document.getElementById("menu");
+    if( ! menu.classList.contains("menu-aberto") ){
+        menu.classList.add("menu-aberto");
+        button.classList.add("botao-aberto");
+    }
+    else {
+        menu.classList.remove("menu-aberto");
+        button.classList.remove("botao-aberto");
+    }
+}
+
 window.addEventListener('load', function() {
     ajustaImagens()
 });
